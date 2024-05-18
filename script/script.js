2f7247eb0414042770a09e3b95116bc64857e1fa
kawrou <121672581+kawrou@users.noreply.github.com>
2024-05-18 14:46:45 +0100

    Refactored math functions

--- script/script.js
+++ script/script.js
@@ -1,17 +1,17 @@
-const { sum, subtract, multiply, divide } = require("./calc_functions.js");
-
 let num1, num2, operator;
 
 const operate = (num1, num2, operator) => {
- switch(operator){
-    case "+":
-        sum(num1,num2);
-    case "-":
-        subtract(num1, num2); 
-    case "*":
-        multiply(num1, num2); 
-    case "/":
-        divide(num1, num2); 
+ switch (operator) {
+  case "+":
+   return num1 + num2;
+  case "-":
+   return num1 - num2;
+  case "*":
+   return num1 * num2;
+  case "/":
+   result = num1 / num2;
+   const pow = Math.pow(10, 7);
+   return Math.round(result * pow) / pow;
  }
 };
 
